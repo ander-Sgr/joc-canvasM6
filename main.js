a31d27dda98166763a796cdfd0665df843e0fa32
anderSgr <andersonbarreto2001@gmail.com>
2023-01-27 19:10:56 +0100

    a

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-
-
-
-window.onload = function (){
-    const backgroundImage = document.getElementById('backgroundGame')
-
-    let canvas = document.getElementById('myCanvas')
-    let ctx
-    ctx = canvas.getContext('2d')
-    ctx.drawImage(backgroundImage, 300,500)
-    draw()
-}
-function draw(){
+const gameComponents = {
 
 }
+const canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+window.onload = function(){
+    drawBackground()
+}
+
+function drawBackground() {
+   imgBackground = new Image()
+   imgBackground.src = './img/backgroundGame.jpg'
+   ctx.drawImage(imgBackground, 0, 0, 300, 150)
+}
+
