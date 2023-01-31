701b3ecfe5ac34671fe8ba150b06d28e71f6aacb
anderSgr <andersonbarreto2001@gmail.com>
2023-01-31 20:01:23 +0100

    work from school

--- main.js
+++ main.js
@@ -1,16 +1,74 @@
-const gameComponents = {
+let canvas = document.getElementById('canvas')
+let ctx = canvas.getContext('2d')
+let up = false
+let down = false
+let left = false
+let right = false
 
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+window.onload = function () {
+    drawBackgroundImage()
+    character.draw()
 }
-const canvas = document.getElementById('canvas')
-ctx = canvas.getContext('2d')
 
-window.onload = function(){
-    drawBackground()
+canvas.addEventListener('keydown', function(event){
+    eventKeyCharacter(event, false)
+})
+
+canvas.addEventListener('keyUp', function(event) {
+    eventKeyCharacter(event, true) 
+})
+
+const character = (x, y) => {
+    this.x = x
+    y: 0,
+    length: 0,
+    speed: 0,
+    draw: () => {
+        let imgCharacter = document.getElementById('characterImage')
+        ctx.drawImage(imgCharacter, 100, 800, 120, 100)
+    }
 }
 
-function drawBackground() {
-   imgBackground = new Image()
-   imgBackground.src = './img/backgroundGame.jpg'
-   ctx.drawImage(imgBackground, 0, 0, 300, 150)
+
+function eventKeyCharacter(eventKey, trigger) {
+    eventKey.preventDefault()
+    switch (eventKey.key) {
+        case 'w':
+        case 'ArrowUp':
+            up = trigger
+            break;
+        case 's':
+        case 'ArrowDoww':
+            down = trigger
+            break;
+        case 'a':
+        case 'ArrowLeft':
+            left = trigger
+            break;
+        case 'd':
+        case 'ArrowRight':
+            right = trigger
+            break
+        default:
+            break;
+    }
 }
 
+function drawBackgroundImage() {
+    let imgBackground = document.getElementById('backgroundGame')
+    ctx.drawImage(imgBackground, 0, 0, window.innerWidth, window.innerHeight)
+}
+
+function main() {
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
